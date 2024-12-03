4778681e709fbba617fea3aedbdebbaa24675cd1
Dynamite <abdmoklss@gmail.com>
2024-12-03 16:07:28 +0300

    "Updated search input field in Admin Dashboard with icon and styling changes"

--- resources/js/Pages/Admin/Dashbord.vue
+++ resources/js/Pages/Admin/Dashbord.vue
@@ -51,12 +51,34 @@ const changePage = (url) => {
     >
         <div class="container w-10/12 mx-auto">
             <!-- حقل البحث -->
-            <div class="mb-4">
+            <div class="mb-4 flex items-center space-x-2">
+                <!-- أيقونة البحث -->
+                <label for="searchKeyword">
+                    <svg
+                        width="3rem"
+                        height="3rem"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="stroke-black dark:stroke-white"
+                    >
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            ></path>
+                        </g>
+                    </svg>
+                </label>
+                <!-- حقل البحث -->
                 <input
+                    id="searchKeyword"
                     type="text"
                     v-model="searchKeyword"
                     placeholder="Search users..."
-                    class="w-full p-2 border border-gray-300 rounded shadow-sm dark:bg-gray-800 dark:text-gray-200"
+                    class="w-full p-2 border border-gray-300 rounded shadow-sm dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
             </div>
 
