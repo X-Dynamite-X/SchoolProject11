fe75927f60a241d63fed3a6bd2c8b796081599ab
Dynamite <abdmoklss@gmail.com>
2024-12-28 21:07:03 +0300

    `Update table styles in DataTable.vue component`

--- resources/js/components/Tabel/DataTable.vue
+++ resources/js/components/Tabel/DataTable.vue
@@ -10,9 +10,9 @@ const emit = defineEmits(["sort"]); // تعريف الحدث 'sort'
 </script>
 
 <template>
-    <table :id="id" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 overflow-scroll  ">
+    <table :id="id" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-300 overflow-scroll  ">
 
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-200">
             <slot name="header" />
         </thead>
         <tbody>
